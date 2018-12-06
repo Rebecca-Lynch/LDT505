dc6aa658118d6cb8b29cd1050c15e69eaf89d3e9
Rebecca-Lynch <45577504+Rebecca-Lynch@users.noreply.github.com>
2018-12-06 13:43:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,7 @@ $( document ).ready(function() {
 	var userInfo = new Array;
 	
 	
-	if (userNumber == 1){
-		var textFile = "https://rebecca-lynch.github.io/LDT505/practice_info.txt";
-	} else if (userNumber == 2){
-		var textFile = "https://rebecca-lynch.github.io/LDT505/practice_info2.txt";
-	}
+
 	
 	
     $.get(textFile, function(data){
